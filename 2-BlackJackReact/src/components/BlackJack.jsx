870d82fefe91bfd81143086b0954b5cdcf523005
Miguecaritatriste <114624729+Miguecaritatriste@users.noreply.github.com>
2024-01-27 01:17:32 +0100

    huahua

--- 2-BlackJackReact/src/components/BlackJack.jsx
+++ 2-BlackJackReact/src/components/BlackJack.jsx
@@ -1,44 +1,53 @@
-import Card from "./Card"
-import cards from "./CardImages"
-import React from "react";
-import { useState } from "react";
-function BlackJack() {
+import Card from "./Card";
+import cards from "./CardImages";
+import { useState, useEffect } from "react";
 
-    const [cardValue, setCardValue] = useState(null);
+function BlackJack() {
+    const randomNumber1 = Math.floor(Math.random() * 48);
+    const [contador, setcontador] = useState(0);
+    const [cartas, setCartas] = useState([{ img: cards[randomNumber1].img, value: cards[randomNumber1].valuee }]);
 
     const handlePedir = () => {
         const randomNumber = Math.floor(Math.random() * 48);
-        setCardValue(randomNumber);
-    }
+
+        setCartas([...cartas, { img: cards[randomNumber].img, value: cards[randomNumber].valuee }]); 
+
+    };
+
+    useEffect(() => {
+        let suma = 0;
+
+        cartas.forEach(carta => {
+            suma = suma + carta.value;
+        });
+
+        setcontador(suma);
+
+        // El código dentro de este bloque se ejecutará después de actualizar el estado
+        console.log(suma);
+    }, [cartas]);
+
     return (
         <div className="w-2/5 border-8 border-solid rounded-lg h-4/5 border-amber-950">
             <div className="flex flex-wrap justify-center w-full h-full p-5 bg-gray-600 bg-opacity-80">
-
-                <div id="cartas-enemigo" className="flex w-4/5 gap-2" >
+                <p className="h-10 w-28 bg-red-400 text-black">
+                    <span>{contador}</span>
+                </p>
+                <div className="flex w-4/5 gap-2">
+                    {/* Renderizar cartas Jugador*/}
+                    {cartas.map((carta, index) => (
+                        <Card key={index} img={carta.img} value={carta.value} />
+                    ))}
                 </div>
-                <p className="w-full text-center">buenas</p>
-                <p className="w-full text-center">buenas</p>
-                <p className="w-full text-center">buenas</p>
-                <div id="cartas-enemigo" className="flex w-4/5 gap-2">
-                    {/* Renderizar la carta directamente en JSX */}
-                    {cardValue !== null && <Card img={cards[cardValue].img} value={cards[cardValue].valuee} />}
-                </div>
-                <p className="w-full text-center">buenas</p>
-                <p className="w-full text-center">buenas</p>
-                <p className="w-full text-center">buenas</p>
-                <p className="w-full text-center">buenas</p>
-                <p className="w-full text-center">buenas</p>
-                <p className="w-full text-center">buenas</p>
-                <p className="w-full text-center">buenas</p>
-
                 <div className="flex justify-between w-3/5 mx-auto">
-                    <button className="w-2/5 bg-gray-200 rounded-lg" onClick={handlePedir}>Pedir carta</button>
-                    <button className="w-2/5 bg-gray-200 rounded-lg ">Plantarse</button>
+                    <button className="w-2/5 bg-gray-200 rounded-lg" onClick={handlePedir}>
+                        Pedir carta
+                    </button>
+                    <button className="w-2/5 bg-gray-200 rounded-lg">Plantarse</button>
                 </div>
-
             </div>
         </div>
-    )
+    );
 }
 
-export default BlackJack
+export default BlackJack;
